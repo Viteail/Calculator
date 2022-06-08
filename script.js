73e67ded6b7bc7458063403e66e63d1106f12ba6
Viteail <vitea.sirbu1@gmail.com>
2022-06-08 17:02:31 +0300

    edit Calculator

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const getSecondDisplay = document.querySelector("#display-second-numbers");
 const getButtons = document.querySelectorAll("button");
 const getButtonAllClear = document.querySelector("#all-clear");
 const getButtonClear = document.querySelector("#clear");
+const mainDisplay = document.querySelector("#display");
 
 let arr = [];
 let secondArr = [];
@@ -16,16 +17,15 @@ function displayNumbers() {
       if (display.matches(".number")) {
         display.addEventListener("click", (e) => {
           if (arr.length !== 29) {
-            getDisplay.textContent = "Not Enough Memory..";
             if (arr.length === 0 && e.target.classList.contains("zero")) {
               return;
             } else arr.push(`${display.value}`);
           }
         });
-        display.addEventListener(
-          "click",
-          () => (getDisplay.textContent = arr.join(""))
-        );
+        display.addEventListener("click", () => {
+          getDisplay.textContent = arr.join("");
+          return console.log(getDisplay.firstChild.textContent);
+        });
       }
     }
   }
@@ -53,9 +53,20 @@ function operate() {
   }
 }
 
+function add() {}
+
+function subtract() {}
+
+function divide() {}
+
+function multiply() {}
+
+function remainder() {}
+
 getButtonAllClear.addEventListener("click", () => {
   arr = [];
   secondArr = [];
+  saveArr = [];
   getDisplay.textContent = arr.join("");
   getSecondDisplay.textContent = secondArr.join("");
 });
@@ -70,3 +81,5 @@ operate();
 
 // ideas :
 // when press operate do arr = saveArr and show saveArr on secondDisplays DONE
+// get numbers from the secondDisplay
+// getDisplay.firstChild.textContent use this method to calculate numbers
